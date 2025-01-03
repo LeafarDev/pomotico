0643179e3c67c1669f7a0992224e2e3777cdc033
Rafael Oliveira da Silva <rafael_oliveira500@hotmail.com>
2025-01-03 18:56:42 -0300

    chore: eslint fix

--- src/atoms/Timer.tsx
+++ src/atoms/Timer.tsx
@@ -3,5 +3,8 @@ import { atomWithStorage } from "jotai/utils";
 
 export const countAtom = atom(0);
 // Atoms with storage
-export const remainingTimeAtom = atomWithStorage("remainingTime", 25 * 60 * 1000); // 25 minutes in milliseconds
+export const remainingTimeAtom = atomWithStorage(
+  "remainingTime",
+  25 * 60 * 1000,
+); // 25 minutes in milliseconds
 export const isRunningAtom = atomWithStorage("isRunning", false);
